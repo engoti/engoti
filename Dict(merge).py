2f2fbedbb5ced6daa73db4da5474efbbf9a0f5e7
engoti <136898202+engoti@users.noreply.github.com>
2024-06-07 14:59:40 +0300

    Create Dict(merge).py

---
+++ Dict(merge).py
@@ -0,0 +1,14 @@
+dict1 =  {'Alex': 5,
+            'Ben': 10,
+            'Eazee': 22,
+            'Silas': 33}
+
+dict2 = {'Sam': 8,
+         'Stephen': 10,
+         'Soliga': 22,
+         'Kylian': 18}
+
+engel = dict1 | dict2
+
+print(engel)
+
