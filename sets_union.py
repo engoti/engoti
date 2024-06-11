7f3486bc1d5d2ad97e774cacf5643af35598d64b
engoti <136898202+engoti@users.noreply.github.com>
2024-06-11 10:12:40 +0300

    Create sets_union.py

---
+++ sets_union.py
@@ -0,0 +1,11 @@
+
+# Python program to modify/add Set elements
+my_set = {10, 20, 30}
+print('Original Set:', my_set)
+
+my_set2 = {40,50,60}
+union= sorted(my_set.union(my_set2))
+
+
+
+print(union)
